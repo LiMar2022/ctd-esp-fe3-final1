5408b51f49d84b25f57ddee9f84ae845dc245ed7
LiMar2022 <originalprogrammer2022>
2024-09-07 19:29:04 -0500

    Iniciando la estructura del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home.jsx';
+import Contact from './Components/Contact.jsx';
+import DentistDetail from './Components/DentistDetail.jsx';
+import Favorites from './Components/Favorites.jsx';
 
 
 function App() {
   return (
+    <BrowserRouter>
       <div className="App">
-          <Navbar/>
-          <Footer/>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contacto" element={<Contact />} />
+          <Route path="/dentista/:id" element={<DentistDetail />} />
+          <Route path="/favs" element={<Favorites />} />
+        </Routes>
+        <Footer />
       </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/DentistDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function DentistDetail() {
+  return <div>Dentist Detail Page</div>;
+}
+
+export default DentistDetail;
